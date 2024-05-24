cb807ffe49f788994fd1fee77630128fb48a710a
Felipe Sutter <lipeps61@gmail.com>
2024-05-24 11:46:50 -0300

    Update ApiConfig.jsx

--- src/config/ApiConfig.jsx
+++ src/config/ApiConfig.jsx
@@ -1,5 +1,5 @@
 const apiLocal = "https://localhost:7191/api/";
-const apiServidor = "http://sistemas.t2mlab.com:8014/api/";
+const apiServidor = "http://t2m-iqs.southcentralus.cloudapp.azure.com:9091/api/";
 
 
 export default { baseURL: `${apiServidor}` }
